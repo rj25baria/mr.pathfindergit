12a4836ef014b225dd96b8c10f9927a491f306ef
Your Name <you@example.com>
2026-01-28 15:42:29 +0530

    update api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const API_URL = import.meta.env.VITE_API_URL || (isLocal ? `http://${window.location.hostname}:5000` : 'https://mr-pathfinder1.onrender.com');
+export const API_URL = import.meta.env.VITE_API_URL || (isLocal ? `http://${window.location.hostname}:5000` : 'https://mr-pathfinder-1-ditt.onrender.com');
